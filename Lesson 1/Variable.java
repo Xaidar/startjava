46fab5fd4f00050c74a11e6bf08dd7160ff9ee8c
Xaidar Myxamatxanov <MukhamatkhanovKK@gmail.com>
2019-03-22 23:16:58 +0000

    Исправлена табуляция

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,22 +1,22 @@
-public class Variable {	
+public class Variable {    
     public static void main(String[] args) {
 
-    	byte numberOfProcessors = 1;
-    	short totalNumberOfCores = 2;
-    	int freeMem = 4;
-    	long totalMem = 8;
-    	float processorSpeed = 2.3f;
-    	double modelId = 14.1d;
-    	char generationIntel = 'I';
-    	boolean charging = true;
+        byte numberOfProcessors = 1;
+        short totalNumberOfCores = 2;
+        int freeMem = 4;
+        long totalMem = 8;
+        float processorSpeed = 2.3f;
+        double modelId = 14.1d;
+        char generationIntel = 'I';
+        boolean charging = true;
 
-		System.out.println("Generation Intel: " + generationIntel);
-		System.out.println("Model ID: " + modelId);
-		System.out.println("Charging: " + charging);
-		System.out.println("Total Memory: " + totalMem);
-		System.out.println("Free Memory: " + freeMem);
-		System.out.println("Number Of Processors: " + numberOfProcessors);
-		System.out.println("Total Number Of Cores: " + totalNumberOfCores);
+        System.out.println("Generation Intel: " + generationIntel);
+        System.out.println("Model ID: " + modelId);
+        System.out.println("Charging: " + charging);
+        System.out.println("Total Memory: " + totalMem);
+        System.out.println("Free Memory: " + freeMem);
+        System.out.println("Number Of Processors: " + numberOfProcessors);
+        System.out.println("Total Number Of Cores: " + totalNumberOfCores);
         System.out.println("Processor Speed: " + processorSpeed);
 
     }
