505a671ae263feaac87abb561d49e9e7414dc493
Xaidar Myxamatxanov <MukhamatkhanovKK@gmail.com>
2019-03-23 13:13:35 +0000

    Unicode.java

---
+++ Lesson 1/Unicode.java
@@ -0,0 +1,10 @@
+public class Unicode {    
+    public static void main(String[] args) {
+
+        for (int i = 9398; i <= 10178; i++) {
+            String s = Character.toString((char)i);
+            System.out.println(s);
+        }
+
+    }
+}
