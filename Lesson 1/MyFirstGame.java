11fd5e2ddea91cc6a4a6f4cf533127e692163238
Xaidar Myxamatxanov <MukhamatkhanovKK@gmail.com>
2019-03-23 13:17:31 +0000

    Исправлена табуляция в MyFirstGame.java

--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -1,27 +1,27 @@
 public class MyFirstGame {    
     public static void main(String[] args) {
 
-    	long userNumber = 38;
-    	long rndNumber = 33;
-    	boolean win = false;
+        long userNumber = 38;
+        long rndNumber = 33;
+        boolean win = false;
 
-    	int i = 0;
-    	while (win == false) {
+        int i = 0;
+        while (win == false) {
 
-    		if (userNumber > rndNumber) {
-    			System.out.println("Введенное вами число больше того, что загадал компьютер");
-    			win = false;
-    			userNumber--;
-    		} else if (userNumber < rndNumber) {
-    			System.out.println("Введенное вами число меньше того, что загадал компьютер");
-    			win = false;
-    			userNumber++;
-    		} else if (userNumber == rndNumber) {
-    			System.out.println("Вы угадали!");
-    			win = true;
-    		}
+            if (userNumber > rndNumber) {
+                System.out.println("Введенное вами число больше того, что загадал компьютер");
+                win = false;
+                userNumber--;
+            } else if (userNumber < rndNumber) {
+                System.out.println("Введенное вами число меньше того, что загадал компьютер");
+                win = false;
+                userNumber++;
+            } else if (userNumber == rndNumber) {
+                System.out.println("Вы угадали!");
+                win = true;
+            }
 
-    	}
+        }
         
     }
 }
